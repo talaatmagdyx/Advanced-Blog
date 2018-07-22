4c472a032ecd8733886ce12bfded7805ec5eefbb
TalaatMagdy <talaatmagdy75@gmail.com>
2018-07-22 10:47:40 +0200

    add style like bootstrape

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  
+ # pages controller
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+  
+ # article Controller
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
