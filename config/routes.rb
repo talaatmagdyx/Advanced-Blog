9c30cecc304f3147cbac1dacbab58ff58189dcb6
TalaatMagdy <talaatmagdy75@gmail.com>
2018-07-24 07:01:13 +0200

    Signup users

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,12 @@ Rails.application.routes.draw do
   
  # article Controller
   resources :articles
+
+ #sessions 
+ get 'signup', to: 'users#new'
+ #post 'users', to: 'users#create'
+
+ #users controller
+ resources :users, except: [:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_22_120128) do
+ActiveRecord::Schema.define(version: 2018_07_22_124527) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2018_07_22_120128) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
